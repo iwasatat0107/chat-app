c30ce47b13d15fa208c0e597f0574a72aa1eba55
iwasatat0107 <it.was07@gmail.com>
2020-10-20 09:34:15 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
     
   end
 
@@ -20,4 +21,5 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
+
 end
